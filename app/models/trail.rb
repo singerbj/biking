2f312ee800d273dee1600a2db3f8e5043d330112
Benjamin Singer <benjaminsinger@Benjamins-MacBook-Pro-3.local>
2014-04-24 20:38:36 -0500

    fixed datetime in trail

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -17,10 +17,15 @@ class Trail < ActiveRecord::Base
         name = r.css('td')[0].children.children.text.to_s
         condition = r.css('td')[1].children.children.text.to_s
         desc = r.css('td')[3].children.text.to_s
-        date_array = r.css('td')[5].children.children.text.to_s.gsub(/[^a-zA-Z0-9: -]/, "").split[0].split('-')
+        date_time_array = r.css('td')[5].children.children.text.to_s.gsub(/[^a-zA-Z0-9: -]/, "").split(' ')
+        
+        date_array = date_time_array[0].split[0].split('-')
+        date_string = (date_array[2] + "-" + date_array[0] + "-" + date_array[1]).to_s
+        time_string = date_time_array[1].to_s                                        
+        tod_string = date_time_array[2].to_s
 
-        date = (date_array[2] + "-" + date_array[0] + "-" + date_array[1]).to_datetime
-
+        date = (date_string + " " + time_string + " " + tod_string).to_datetime
+        
         if t = Trail.where(:name => name)[0]
           t.update_attributes!(:name => name, :condition => condition, :desc => desc, :date => date)
         else
